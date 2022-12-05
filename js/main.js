ddd4f0867baddaca60cc3b2fc4af8173a9afcccb
mahmoud ramadan <mahmoudr03@gmail.com>
2022-12-06 01:04:25 +0200

    coding

--- js/main.js
+++ js/main.js
@@ -65,31 +65,53 @@ var swiper = new Swiper(".package", {
     }
 });
 
+$(document).ready(()=>{
+    let isMenuAlreadyOpen = false;
+    $('.navbar-burger').on('click',()=>{
+          $('body').css("overflow",isMenuAlreadyOpen?"auto":"hidden")
+          isMenuAlreadyOpen = !isMenuAlreadyOpen
+    })
+  });
 
 // Network Section Slider
-// var swiper = new Swiper(".network", {
-//     slidesPerView: 5,
-//     spaceBetween: 30,
-//     pagination: {
-//         el: ".swiper-pagination",
-//         clickable: true,
-//     },
-//     breakpoints: {
-//         320: {
-//             slidesPerView: 1,
-//             spaceBetween: 10
-//         },
-//         576: {
-//             slidesPerView: 2,
-//             spaceBetween: 30
-//         },
-//         767: {
-//             slidesPerView: 3,
-//             spaceBetween: 20
-//         },
-//         992: {
-//             slidesPerView: 5,
-//             spaceBetween: 20
-//         }
-//     }
-// });
+var swiper = new Swiper(".network", {
+    slidesPerView: 5,
+    spaceBetween: 30,
+    pagination: {
+        el: ".swiper-pagination-1",
+        clickable: true,
+    },
+    breakpoints: {
+        320: {
+            slidesPerView: 1,
+            spaceBetween: 10
+        },
+        576: {
+            slidesPerView: 2,
+            spaceBetween: 30
+        },
+        767: {
+            slidesPerView: 3,
+            spaceBetween: 20
+        },
+        992: {
+            slidesPerView: 5,
+            spaceBetween: 20
+        }
+    }
+});
+
+jQuery(function ($) {
+	var $active = $('#accordion .panel-collapse.in').prev().addClass('active');
+	$active.find('a').append('<span class="glyphicon glyphicon-minus pull-right"></span>');
+	$('#accordion .panel-heading').not($active).find('a').prepend('<span class="glyphicon glyphicon-plus pull-right"></span>');
+	$('#accordion').on('show.bs.collapse', function (e)
+	{
+		$('#accordion .panel-heading.active').removeClass('active').find('.glyphicon').toggleClass('glyphicon-plus glyphicon-minus');
+		$(e.target).prev().addClass('active').find('.glyphicon').toggleClass('glyphicon-plus glyphicon-minus');
+	});
+	$('#accordion').on('hide.bs.collapse', function (e)
+	{
+		$(e.target).prev().removeClass('active').find('.glyphicon').removeClass('glyphicon-minus').addClass('glyphicon-plus');
+	});
+});
