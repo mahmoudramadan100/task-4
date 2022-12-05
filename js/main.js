ecd5b59c3db844c0b101eee6886c785a988be17b
mahmoud ramadan <mahmoudr03@gmail.com>
2022-12-05 02:38:52 +0200

    codeing

--- js/main.js
+++ js/main.js
@@ -1,26 +1,28 @@
+// $(function () {
+//     $('.nav-mobile').on('click', function () {
+//         $(".navbar")
+//             .css('display', 'flex')
+//             .fadeIn('slow');
+//     });
 
-
-// $(function() {     
-//   $('.nav-mobile').on('click',function() {
-//      $(".navbar").css(
-//       'display', 'flex'
-//      ).fadeIn('slow');
-//   });
-
-//   $('.nav-close').on('click',function() {
-//     $(".navbar").css(
-//      'display', 'none'
-//     ).fadeOut();
-//  });
+//     $('.nav-close').on('click', function () {
+//         $(".navbar")
+//             .css('display', 'none')
+//             .fadeOut();
+//     });
 // });
 
 // const navbar = document.querySelector('.navbar');
 
 // window.onscroll = () => {
 //     if (window.scrollY > 550) {
-//         navbar.classList.add('navbar-scroll');
+//         navbar
+//             .classList
+//             .add('navbar-scroll');
 //     } else {
-//         navbar.classList.remove('navbar-scroll');
+//         navbar
+//             .classList
+//             .remove('navbar-scroll');
 //     }
 // };
 
@@ -28,6 +30,60 @@ var swiper = new Swiper(".slider", {
     cssMode: true,
     navigation: {
         nextEl: ".swiper-button-next",
-        prevEl: ".swiper-button-prev",
+        prevEl: ".swiper-button-prev"
+    }
+});
+
+var swiper = new Swiper(".package", {
+    slidesPerView: 4,
+    spaceBetween: 30,
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
     },
+    breakpoints: {
+        320: {
+            slidesPerView: 1,
+            spaceBetween: 30
+        },
+        576: {
+            slidesPerView: 2,
+            spaceBetween: 30
+        },
+        767: {
+            slidesPerView: 3,
+            spaceBetween: 20
+        },
+        992: {
+            slidesPerView: 4,
+            spaceBetween: 20
+        }
+    }
+});
+
+var swiper = new Swiper(".network", {
+    slidesPerView: 5,
+    spaceBetween: 30,
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+    },
+    breakpoints: {
+        320: {
+            slidesPerView: 1,
+            spaceBetween: 10
+        },
+        576: {
+            slidesPerView: 2,
+            spaceBetween: 30
+        },
+        767: {
+            slidesPerView: 3,
+            spaceBetween: 20
+        },
+        992: {
+            slidesPerView: 5,
+            spaceBetween: 20
+        }
+    }
 });
