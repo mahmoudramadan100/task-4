d8bb995c65afd62a48c43621348ffe63e1969d8e
mahmoud ramadan <mahmoudr03@gmail.com>
2022-12-06 01:06:23 +0200

    coding

--- js/main.js
+++ js/main.js
@@ -1,17 +1,3 @@
-// $(function () {
-//     $('.nav-mobile').on('click', function () {
-//         $(".navbar")
-//             .css('display', 'flex')
-//             .fadeIn('slow');
-//     });
-
-//     $('.nav-close').on('click', function () {
-//         $(".navbar")
-//             .css('display', 'none')
-//             .fadeOut();
-//     });
-// });
-
 // const navbar = document.querySelector('.navbar');
 
 // window.onscroll = () => {
@@ -68,10 +54,10 @@ var swiper = new Swiper(".package", {
 $(document).ready(()=>{
     let isMenuAlreadyOpen = false;
     $('.navbar-burger').on('click',()=>{
-          $('body').css("overflow",isMenuAlreadyOpen?"auto":"hidden")
-          isMenuAlreadyOpen = !isMenuAlreadyOpen
+        $('body').css("overflow",isMenuAlreadyOpen?"auto":"hidden")
+        isMenuAlreadyOpen = !isMenuAlreadyOpen
     })
-  });
+});
 
 // Network Section Slider
 var swiper = new Swiper(".network", {
@@ -101,17 +87,3 @@ var swiper = new Swiper(".network", {
     }
 });
 
-jQuery(function ($) {
-	var $active = $('#accordion .panel-collapse.in').prev().addClass('active');
-	$active.find('a').append('<span class="glyphicon glyphicon-minus pull-right"></span>');
-	$('#accordion .panel-heading').not($active).find('a').prepend('<span class="glyphicon glyphicon-plus pull-right"></span>');
-	$('#accordion').on('show.bs.collapse', function (e)
-	{
-		$('#accordion .panel-heading.active').removeClass('active').find('.glyphicon').toggleClass('glyphicon-plus glyphicon-minus');
-		$(e.target).prev().addClass('active').find('.glyphicon').toggleClass('glyphicon-plus glyphicon-minus');
-	});
-	$('#accordion').on('hide.bs.collapse', function (e)
-	{
-		$(e.target).prev().removeClass('active').find('.glyphicon').removeClass('glyphicon-minus').addClass('glyphicon-plus');
-	});
-});
